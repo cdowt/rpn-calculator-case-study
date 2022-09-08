7f20a17d4457e92d8e895358563cbd88be22a00a
Camden Dixie O'Brien <cobrien@witekio.com>
2022-09-08 15:13:18 +0100

    Rust: configure serial etc in main()

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+extern crate cortex_m_rt;
+extern crate panic_halt;
+extern crate stm32f4xx_hal;
+
+use core::fmt::Write;
+use cortex_m_rt::entry;
+use stm32f4xx_hal::{pac::Peripherals, prelude::*, serial};
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take().unwrap();
+
+    let gpioa = peripherals.GPIOA.split();
+    let tx = gpioa.pa0.into_alternate::<8>();
+    let rx = gpioa.pa1.into_alternate::<8>();
+
+    let clocks = peripherals.RCC.constrain().cfgr.freeze();
+    let config = serial::config::Config::default();
+    let mut serial = serial::Serial::new(peripherals.UART4, (tx, rx), config, &clocks).unwrap();
+
+    serial.write_str("Hello, world!\r\n").unwrap();
+
+    loop {}
 }
