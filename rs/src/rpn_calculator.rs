30ae5e44dbd904485c73626ec7579d7cdbbb4ae2
Camden Dixie O'Brien <cobrien@witekio.com>
2022-09-08 18:16:01 +0100

    Rust: write top level of read_expression()

--- rs/src/rpn_calculator.rs
+++ rs/src/rpn_calculator.rs
@@ -7,9 +7,12 @@ enum Term {
 }
 
 enum Error {
+    InvalidTerm,
+    TooManyTerms,
 }
 
 const MAX_TERMS: usize = 32;
+const MAX_TOKEN_LENGTH: usize = 16;
 
 const PROMPT: &'static str = "> ";
 
@@ -34,7 +37,26 @@ fn read_expression(
     input: &mut impl Read<u8>,
     terms: &mut [Term; MAX_TERMS],
 ) -> Result<usize, Error> {
-    todo!();
+    let mut token: [u8; MAX_TOKEN_LENGTH] = [0; MAX_TOKEN_LENGTH];
+    let mut number_of_terms = 0;
+    loop {
+        let (token_length, end_of_line) = read_token(input, &mut token)?;
+        let term = read_operator(&token, token_length)
+            .or(read_value(&token, token_length))
+            .ok_or(Error::InvalidTerm)?;
+
+        if number_of_terms == MAX_TERMS {
+            return Err(Error::TooManyTerms);
+        } else {
+            terms[number_of_terms] = term;
+            number_of_terms += 1;
+        }
+
+        if end_of_line {
+            break;
+        }
+    }
+    Ok(number_of_terms)
 }
 
 fn evaluate(terms: &[Term; MAX_TERMS], number_of_terms: usize) -> Result<isize, Error> {
@@ -46,5 +68,20 @@ fn print_result(result: isize, output: &mut impl Write<u8>) {
 }
 
 fn print_error(error: Error, output: &mut impl Write<u8>) {
+    todo!();
+}
+
+fn read_token(
+    input: &mut impl Read<u8>,
+    token: &mut [u8; MAX_TOKEN_LENGTH],
+) -> Result<(usize, bool), Error> {
+    todo!();
+}
+
+fn read_operator(token: &[u8; MAX_TOKEN_LENGTH], token_length: usize) -> Option<Term> {
+    todo!();
+}
+
+fn read_value(token: &[u8; MAX_TOKEN_LENGTH], token_length: usize) -> Option<Term> {
     todo!();
 }
