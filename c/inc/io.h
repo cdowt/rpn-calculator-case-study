d9e104ecc5d3633466011d6a3332a89b7f21225f
Camden Dixie O'Brien <cobrien@witekio.com>
2022-09-09 19:20:32 +0100

    C: define print_str() function

--- c/inc/io.h
+++ c/inc/io.h
@@ -1,6 +1,7 @@
 #ifndef IO_H
 #define IO_H
 
+void print_str(const char *str);
 void print_char(char c);
 char read_char(void);
 
--- c/src/io.c
+++ c/src/io.c
@@ -2,6 +2,12 @@
 
 #include "stm32f4xx_usart.h"
 
+void print_str(const char *str)
+{
+	for (const char *p = str; *p != '\0'; ++p)
+		print_char(*p);
+}
+
 void print_char(char c)
 {
     USART_SendData(UART4, (uint16_t)c);
