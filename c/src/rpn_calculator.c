f5c41efeb142c37f72cb949f5d69800b2f0db63d
Camden Dixie O'Brien <cobrien@witekio.com>
2022-09-09 19:35:45 +0100

    C: define data structures

--- c/src/rpn_calculator.c
+++ c/src/rpn_calculator.c
@@ -1,5 +1,37 @@
 #include "rpn_calculator.h"
 
+#define MAX_TERMS 32
+
+enum error {
+	NO_ERROR,
+};
+
+enum term_type {
+	VALUE,
+	OPERATOR,
+};
+
+enum operator{
+	PLUS,
+	MINUS,
+	MULTIPLY,
+	DIVIDE,
+	REMAINDER,
+};
+
+struct term {
+	enum term_type type;
+	union {
+		int value;
+		enum operator operator;
+	};
+};
+
+struct expression {
+	struct term terms[MAX_TERMS];
+	unsigned term_count;
+};
+
 void run_repl(void)
 {
 }
